54fd05e5205630adad8efb84456cea421cd2cc0c
Peyton <peytonscherschel@gmail.com>
2025-07-07 15:06:17 -0700

    mint changes

--- nvim/lua/peyton/plugins/graple.lua
+++ nvim/lua/peyton/plugins/graple.lua
@@ -7,19 +7,19 @@ return {
         style = "basename",
     },
     keys = {
-        { "<leader>a", "<cmd>Grapple tag<cr>",                      desc = "Tag a file" },
+        { "<leader>bb", "<cmd>Grapple tag<cr>",                      desc = "Tag a file" },
         -- { "<leader>d", "<cmd>Grapple untag<cr>",                    desc = "Untag a file" },
-        { "<leader>T", "<cmd>Grapple toggle scope=global<cr>",      desc = "Tag a file globally" },
-        { "<c-e>",     "<cmd>Grapple toggle_tags<cr>",              desc = "Toggle tags menu" },
-        { "<c-w>",     "<cmd>Grapple toggle_tags scope=global<cr>", desc = "Toggle tags menu" },
+        { "<leader>bB", "<cmd>Grapple toggle scope=global<cr>",      desc = "Tag a file globally" },
+        { "<leader>bs", "<cmd>Grapple toggle_tags<cr>",              desc = "Toggle tags menu" },
+        { "<c-w>",      "<cmd>Grapple toggle_tags scope=global<cr>", desc = "Toggle tags menu" },
 
-        { "<leader>j", "<cmd>Grapple select index=1<cr>",           desc = "Select first tag" },
-        { "<leader>h", "<cmd>Grapple select index=2<cr>",           desc = "Select second tag" },
-        { "<leader>k", "<cmd>Grapple select index=3<cr>",           desc = "Select third tag" },
-        { "<leader>l", "<cmd>Grapple select index=4<cr>",           desc = "Select fourth tag" },
+        { "<leader>j",  "<cmd>Grapple select index=1<cr>",           desc = "Select first tag" },
+        { "<leader>h",  "<cmd>Grapple select index=2<cr>",           desc = "Select second tag" },
+        { "<leader>k",  "<cmd>Grapple select index=3<cr>",           desc = "Select third tag" },
+        { "<leader>l",  "<cmd>Grapple select index=4<cr>",           desc = "Select fourth tag" },
 
-        { "<c-s-n>",   "<cmd>Grapple cycle_tags next<cr>",          desc = "Go to next tag" },
-        { "<c-s-p>",   "<cmd>Grapple cycle_tags prev<cr>",          desc = "Go to previous tag" },
+        { "<c-s-n>",    "<cmd>Grapple cycle_tags next<cr>",          desc = "Go to next tag" },
+        { "<c-s-p>",    "<cmd>Grapple cycle_tags prev<cr>",          desc = "Go to previous tag" },
     },
     ---Additional window options for Grapple windows
     ---See :h nvim_open_win
---
+++ settup-list.md
@@ -0,0 +1,13 @@
+# tmux
+sudo apt-get install tmux
+
+# npm 
+sudo apt-get install npm 
+
+# nvim 
+
+# i3
+
+# wezterm
+
+
